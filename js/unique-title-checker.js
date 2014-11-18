815fe15905ea1e23faa8dbfd64b4ac6a90b366e7
BA <kau@connecticum.de>
2014-11-18 15:29:39 +0100

    show no warning on empty titles

--- js/unique-title-checker.js
+++ js/unique-title-checker.js
@@ -2,12 +2,19 @@ jQuery( document ).ready( function ( $ ) {
 
 	$( '#title' ).blur( function () {
 
+		var title = $( this ).val();
+
+		// show no warning on empty titles
+		if ( '' == title ) {
+			return;
+		}
+
 		var request_data = {
 			action: 'unique_title_check',
 			ajax_nonce: unique_title_checker.nonce,
 			post__not_in: [ $( '#post_ID' ).val() ],
 			post_type: $( '#post_type' ).val(),
-			post_title: $( this ).val()
+			post_title: title
 		};
 
 		$.ajax( {
