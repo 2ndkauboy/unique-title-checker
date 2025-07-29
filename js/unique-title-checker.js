39f77361264709d6b7c9cfc4493ed5e5a6493612
Bernhard Kau <github@kau-boys.de>
2025-07-29 16:06:04 +0200

    Fix some linting issues

--- js/unique-title-checker.js
+++ js/unique-title-checker.js
@@ -4,40 +4,47 @@
  * @package unique-title-checker
  */
 
-document.addEventListener( "DOMContentLoaded", function () {
-	document.getElementById( "title" ).addEventListener( "blur", function () {
-		var title = this.value;
+document.addEventListener( 'DOMContentLoaded', function () {
+	document.getElementById( 'title' ).addEventListener( 'blur', function () {
+		const title = this.value;
 
 		// Show no warning on empty titles.
-		if (title === "") {
+		if (title === '') {
 			return;
 		}
 
-		var requestData = new URLSearchParams( {
-			action: "unique_title_check",
+		const postId = document.querySelector( '#post_ID' )?.value;
+		const postType = document.querySelector( '#post_type' )?.value;
+
+		// Build the request payload
+		const requestData = new URLSearchParams( {
+			action: 'unique_title_check',
 			ajax_nonce: unique_title_checker.nonce,
-			post_id: document.getElementById( "post_ID" ).value,
-			post_type: document.getElementById( "post_type" ).value,
+			post_id: postId || '',
+			post_type: postType || '',
 			post_title: title
 		} );
 
-		fetch( ajaxurl + "?" + requestData.toString() )
-			.then( response => response.json() )
-			.then( data => {
-				var messageElement = document.getElementById( "unique-title-message" );
+		// Perform the AJAX request
+		fetch( ajaxurl + '?' + requestData.toString() )
+			.then( (response) => response.json() )
+			.then( (data) => {
+				const messageElement = document.getElementById( 'unique-title-message' );
 				if (messageElement) {
 					messageElement.remove();
 				}
 
-				if (data.status === "error" || !unique_title_checker.only_unique_error) {
-					document.getElementById( "post" ).insertAdjacentHTML(
-						"beforebegin",
+				if (data.status === 'error' || !unique_title_checker.only_unique_error) {
+					document.getElementById( 'post' ).insertAdjacentHTML(
+						'beforebegin',
 						`<div id="unique-title-message" class="${ data.status }">
 							<p>${ data.message }</p>
 						</div>`
 					);
 				}
 			} )
-			.catch( error => console.error( "Error fetching unique title check:", error ) );
+			.catch( (error) => {
+				console.error( 'Error fetching unique title check:', error );
+			});
 	} );
 } );
